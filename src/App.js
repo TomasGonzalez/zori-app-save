c50dc136ace40dd6d25e6092575d09ed8d5f3926
Tomas Gonzalez <tomasgp14@hotmail.com>
2020-03-25 13:59:02 -0400

    Changed root directory

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// import test from "~/components/test";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className=' App-header'>
+        <p>Zori</p>
+        {/* <test /> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
