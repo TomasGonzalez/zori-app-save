8df6a7d56fd352e6f5881b21a332ac8f7cbf6261
Tomas Gonzalez <tomasgp14@hotmail.com>
2020-03-30 16:56:27 -0400

    removed .env and adde djsconfig.json

---
+++ build/precache-manifest.8e0952478de556e28a8d496d51d23add.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20239d7d1ee1ba6d9e4e14c1e2c524d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "715b2299d1594390639a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1a7db004532c19be2168",
+    "url": "/static/js/2.b858223e.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.b858223e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "715b2299d1594390639a",
+    "url": "/static/js/main.b2562150.chunk.js"
+  },
+  {
+    "revision": "0d6611f675776f91185c",
+    "url": "/static/js/runtime-main.8fbbae39.js"
+  },
+  {
+    "revision": "3062eb0a3b9561d1c2cd11f7be9b62a8",
+    "url": "/static/media/login-side-image.3062eb0a.png"
+  }
+]);
