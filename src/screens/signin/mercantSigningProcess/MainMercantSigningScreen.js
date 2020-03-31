6181dec7bbe77817524ae7850a9fc4d40380dd54
Tomas Gonzalez <tomasgp14@hotmail.com>
2020-03-31 03:20:46 -0400

    sign in process almost completed

--- src/screens/signin/mercantSigningProcess/MainMercantSigningScreen.js
+++ src/screens/signin/mercantSigningProcess/MainMercantSigningScreen.js
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 
 import styled, { withTheme } from "styled-components";
 import { MdClose } from "react-icons/md";
-
 import Progress from "react-progress";
+
+import SignUpForm from "./SignUpForm";
 
 const Header = styled.div`
   width: 100%;
@@ -27,8 +28,36 @@ const Logo = styled.img`
   padding-left: 16px;
 `;
 
+const ProgressBar = props => {
+  return (
+    <div>
+      <div
+        style={{
+          position: "relative",
+          height: 3,
+          top: 14,
+          width: "100%",
+          backgroundColor: props.theme.color.lightGray
+        }}
+      />
+      <Progress
+        style={{
+          height: 3,
+          top: 0,
+          position: "relative",
+          boxShadow: "transparent"
+        }}
+        color={props.theme.color.green1}
+        height={3}
+        speed={0.4}
+        percent={props.progress}
+      />
+    </div>
+  );
+};
+
 function MainMercantSigninScreen(props) {
-  const [progress, setProgress] = useState(0);
+  const [progress, setProgress] = useState(10);
 
   return (
     <>
@@ -38,29 +67,8 @@ function MainMercantSigninScreen(props) {
           <MdClose onClick={props.onRequestClose} size={20} />
         </Close>
       </Header>
-      <div>
-        <div
-          style={{
-            position: "relative",
-            height: 3,
-            top: 14,
-            width: "100%",
-            backgroundColor: props.theme.color.lightGray
-          }}
-        />
-        <Progress
-          style={{
-            height: 3,
-            top: 0,
-            position: "relative",
-            boxShadow: "transparent"
-          }}
-          color={props.theme.color.green1}
-          height={3}
-          speed={0.4}
-          percent={progress}
-        />
-      </div>
+      <ProgressBar progress={progress} {...props} />
+      <SignUpForm />
     </>
   );
 }
---
+++ src/screens/signin/mercantSigningProcess/SignUpForm.js
@@ -0,0 +1,121 @@
+import React from "react";
+
+import styled from "styled-components";
+import { Form, Field } from "react-final-form";
+import { TextInput, PasswordInput } from "components/forms/inputs";
+
+import { EmailValidator } from "lib/formValidation";
+import Title from "components/Title";
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ImageContainer = styled.img`
+  height: 370.4px;
+  width: 400px;
+
+  @media (max-width: 1024px) {
+    display: none;
+  
+`;
+
+const FormContaienr = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${props => props.theme.color.white};
+  height: 100vh;
+  overflow: auto;
+  ::-webkit-scrollbar {
+    width: 0px; /* Remove scrollbar space */
+    background: transparent; /* Optional: just make scrollbar invisible */
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: #ff0000;
+  }
+`;
+
+const StyledForm = styled.form`
+  padding: 80px;
+`;
+
+const NamesWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const FormWrapper = styled.div`
+  max-width: 552px;
+`;
+
+export default function SignUpForm() {
+  return (
+    <MainContainer>
+      <ImageContainer
+        alt='People working'
+        src={require("assets/people-working.png")}
+      />
+      <FormContaienr>
+        <Form
+          onSubmit={() => {}}
+          render={({ handleSubmit }) => (
+            <StyledForm>
+              <Title
+                style={{ marginBottom: 56 }}
+                errorMessage={""}
+                label={"Sign up and change how you sell online with ZORI"}
+              />
+              <FormWrapper>
+                <NamesWrapper>
+                  <Field
+                    name='firstName'
+                    component={TextInput}
+                    style={{ marginBottom: 64, width: "42%" }}
+                    placeholder='Enter your first name'
+                  />
+                  <Field
+                    name='lastName'
+                    component={TextInput}
+                    style={{ marginBottom: 64, width: "42%" }}
+                    placeholder='Enter your last name'
+                  />
+                </NamesWrapper>
+                <Field
+                  name='email'
+                  component={TextInput}
+                  validate={EmailValidator}
+                  style={{ marginBottom: 64, width: "100%" }}
+                  placeholder='Enter your e-mail address'
+                />
+                <Field
+                  name='phone'
+                  component={TextInput}
+                  style={{ marginBottom: 64, width: "100%" }}
+                  placeholder='Enter your phone number'
+                />
+                <Field
+                  name='password'
+                  component={TextInput}
+                  style={{ marginBottom: 64, width: "100%" }}
+                  placeholder='Create your password'
+                />
+                <Field
+                  name='verifyPassword'
+                  component={TextInput}
+                  style={{ marginBottom: 64, width: "100%" }}
+                  placeholder='Verify your password'
+                />
+              </FormWrapper>
+            </StyledForm>
+          )}
+        />
+      </FormContaienr>
+    </MainContainer>
+  );
+}
